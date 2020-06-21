423516891a460bf39d912bd9f71d506a1deb32c7
zhangningle <1160030724@qq.com>
2020-06-21 23:14:18 +0800

    学习二叉搜索树

---
+++ data-structure/《学习JavaScript数据结构和算法》笔记/BinarySearchTree.js
@@ -0,0 +1,40 @@
+function BinarySearchTree(){
+    var root = null;
+    var Node = function(key){
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+    // 向树中插入一个新的键
+    this.insert = function(key){
+
+    }
+    // 搜索键，如果存在返回true，不存在返回false
+    this.search = function(key){
+
+    }
+    // 通过中序遍历所有节点
+    this.inOrderTraverse = function(){
+
+    }
+    // 通过先序遍历所有节点
+    this.perOrderTraverse = function(){
+
+    }
+    // 通过后序遍历所有节点
+    this.postOrderTraverse = function(){
+
+    }
+    // 返回树中最下的值/键
+    this.min = function(){
+
+    }
+    // 返回树中最大的值
+    this.max = function(){
+        
+    }
+    // 从树中移除某个键
+    this.remove = function(key){
+        
+    }
+}
